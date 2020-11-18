c08c8b3ef15a680e69b79c6803d247e223032558
Kendra G <glassproud2be@yahoo.com>
2020-11-17 22:40:05 -0600

    update

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-$(document).ready(function() {
-  window.onload = function() {
+$(document).ready(function () {
+  console.log("RUN ON START")
+  window.onload = function () {
     localStorage.getItem("cityInfo");
   }
 
   function citySearch() {
 
-  var cityName = $("#city-name").val().trim();
-  var apiKey = "e56db2c0cbedff8b95a7a0ec30f3b548";
-  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" 
+    var cityName = $("#city-name").val().trim();
+    var apiKey = "e56db2c0cbedff8b95a7a0ec30f3b548";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
 
-  $.ajax({
-
-    url: queryURL + cityName + "&appid=" + apiKey,
-    method: "GET"
-  })
-    .then(function(response) {
-
-    });
+    $.ajax({
+      url: queryURL + cityName + "&appid=" + apiKey,
+      method: "GET"
+    })
+      .then(function (response) {
+        // here on out
+        console.log(response);
+      });
   }
-$("#button").on("click", function(){
-
+  $("#button").on("click", function () {
+    console.log("this is working")
+    citySearch();
+  });
 });
-}
